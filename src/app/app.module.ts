c9553d11d882b85df300eede6359db8f08e0b87f
viraca ericka <evDev02@gamil.com>
2019-03-07 10:47:27 -0400

    adding principalNavBar and PrincipalFooter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PrincipalPageComponent } from './principal-page/principal-page.component';
+import { PrincipalNavBarComponent } from './principal-nav-bar/principal-nav-bar.component';
+import { PrincipalFooterComponent } from './principal-footer/principal-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PrincipalPageComponent
+    PrincipalPageComponent,
+    PrincipalNavBarComponent,
+    PrincipalFooterComponent
   ],
   imports: [
     BrowserModule
